77dc1115852d98d165c7bb159411fe61021ec839
gnurayn <149106331+gnurayn@users.noreply.github.com>
2025-12-04 02:14:00 -0500

    add id

--- Kambaz/Quizzes/schema.js
+++ Kambaz/Quizzes/schema.js
@@ -2,6 +2,7 @@ import mongoose from "mongoose";
 
 const quizSchema = new mongoose.Schema(
     {
+        _id: { type: String },
         title: { type: String, required: true },
         course: { type: String, required: true },
         description: String,
@@ -25,7 +26,8 @@ const quizSchema = new mongoose.Schema(
         published: { type: Boolean, default: false },
         questionCount: { type: Number, default: 0 }
     },
-    { collection: "quizzes" }
+    { collection: "quizzes",
+        _id: false }
 );
 
 export default quizSchema;
